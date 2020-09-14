3b0b3ab6648874ff6d9d382c3b7d91169ea2c325
Regilith <buldetbabe@gmail.com>
2020-09-14 14:35:46 -0600

    "update to zinit"

--- .config/zsh.d/zshrc.d/zinit.zsh
+++ .config/zsh.d/zshrc.d/zinit.zsh
@@ -2,16 +2,21 @@ declare -A ZINIT
 export ZINIT[HOME_DIR]="$ZDOTDIR/zinit"
 export ZINIT[BIN_DIR]="$ZDOTDIR/zinit/bin"
 export ZINIT[ZCOMPDUMP_PATH]="$ZDOTDIR/.zcompdump"
+### Added by Zinit's installer
+if [[ ! -f "$ZINIT[BIN_DIR]/zinit.zsh" ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
+    command mkdir -p "$ZINIT[HOME_DIR]" && command chmod g-rwX "$ZINIT[HOME_DIR]"
+    command git clone https://github.com/zdharma/zinit "$ZINIT[BIN_DIR]" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f"
+fi
+source "$ZINIT[BIN_DIR]/zinit.zsh"
+### End of Zinit installer's chunk
 
-source $ZDOTDIR/zinit/bin/zinit.zsh
-#autoload -Uz _zinit
-#(( ${+_comps} )) && _comps[zinit]=_zinit
 
-## TODO: implement turbo mode
 #PS1="~ >" # provide a simple prompt till the theme loads
 zinit ice wait'!' lucid
 zinit ice depth=1; zinit light romkatv/powerlevel10k # official startup
-
 
 
 #zinit light zsh-users/zsh-completions
